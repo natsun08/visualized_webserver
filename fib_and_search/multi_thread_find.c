403aaaae1aad8f4dd03b156f1352e6a2c8af9200
Kourtney Ru <145283815+KourtneyRu23@users.noreply.github.com>
2024-12-13 10:23:08 +0700

    Update multi_thread_find.c

--- fib_and_search/multi_thread_find.c
+++ fib_and_search/multi_thread_find.c
@@ -1,52 +1,40 @@
+#include <stdio.h>
 #include <pthread.h>
 #include <stdatomic.h>
 #include "multi_thread_find.h"
 #include "common.h"
-#include <stdio.h>
-
+// Max number of threads to create
 #define thread_max 4
+
+// Flag to indicate if key is found in a[]
+// or not.
 atomic_int f = 0;
 int current_thread = 0;
 
-void* ThreadSearch(void* args) {
+// Linear search function which will
+// run for all the threads
+void* ThreadSearch(void* args)
+{
   int num = current_thread++;
   int size_per_thread = searchInput.size / thread_max;
   
-  for (int i = num * size_per_thread; i < ((num + 1) * size_per_thread) && i < searchInput.size; i++) {
+  for (int i = num * size_per_thread;
+       i < ((num + 1) * size_per_thread) && i < searchInput.size; i++) {
     if (f == 1) {
-      return NULL; // Early exit
+      return NULL; // Early exit if key is already found
     }
     if (searchInput.array[i] == searchInput.key) {
-      f = 1; // Key found
+      f = 1;
       return NULL;
     }
   }
   return NULL;
 }
 
-int MultiThreadSearch(char* result) {
-  pthread_t threads[thread_max];
-  current_thread = 0;
-  f = 0; // Reset flag before starting the search
-  
-  // Create threads
-  for (int i = 0; i < thread_max; i++) {
-    pthread_create(&threads[i], NULL, ThreadSearch, NULL);
-  }
-  
-  // Wait for threads to finish
-  for (int i = 0; i < thread_max; i++) {
-    pthread_join(threads[i], NULL);
-  }
-  
-  // Log and populate the result
-  if (f == 1) {
-    sprintf(result, "Key %d found (Multi-threaded)", searchInput.key);
-    printf("%s\n", result); // Log result to server
-    return 1;
-  } else {
-    sprintf(result, "Key %d not found (Multi-threaded)", searchInput.key);
-    printf("%s\n", result); // Log result to server
-    return 0;
-  }
+// Function to output the result of the search
+void OutputResult() {
+  if (f == 1)
+    printf("Key element found\n");
+  else
+    printf("Key not present\n");
 }
